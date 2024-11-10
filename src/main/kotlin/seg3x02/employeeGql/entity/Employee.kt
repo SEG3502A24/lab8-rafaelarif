6afed169c2103295c0f0f8b683beafe2828a205e
Rafael Arif <rafael.arif@outlook.com>
2024-11-10 14:33:08 -0500

    Completed GraphQL schema and resolver code

--- src/main/kotlin/seg3x02/employeeGql/entity/Employee.kt
+++ src/main/kotlin/seg3x02/employeeGql/entity/Employee.kt
@@ -5,13 +5,12 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "employee")
 data class Employee(
+        @Id
+        var id: String? = null,
         val name: String,
         val dateOfBirth: String,
         val city: String,
         val salary: Float,
         val gender: String?,
         val email: String?
-) {
-    @Id
-    var id: String = ""
-}
+)
--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,42 @@
 package seg3x02.employeeGql.resolvers
 
 import org.springframework.stereotype.Controller
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver @Autowired constructor(
+    private val employeesRepository: EmployeesRepository
+){
 
+@QueryMapping
+    fun employees(): List<Employee> 
+    {
+        return employeesRepository.findAll()
+    }
+
+    @QueryMapping
+    fun employeeById(@Argument id: String): Employee? 
+    {
+        return employeesRepository.findById(id).orElse(null)
+    }
+
+    @MutationMapping
+    fun createEmployee(@Argument input: CreateEmployeeInput): Employee 
+    {
+        val employee = Employee(
+            name = input.name ?: "",
+            dateOfBirth = input.dateOfBirth ?: "",
+            city = input.city ?: "",
+            salary = input.salary ?: 0.0f,
+            gender = input.gender,
+            email = input.email
+        )
+        return employeesRepository.save(employee)
+    }
 }
